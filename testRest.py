af9939770d68697b10ca0f11d9cb0c6de9333f01
Arun Selvamani <ASelvamani@walmartlabs.com>
2017-06-11 19:23:23 -0700

    timed call

--- testRest.py
+++ testRest.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
+from time import sleep
 import apiCall
-apiCall.postConditions(22,55,253,500)
+
+while True:
+	try:
+		apiCall.postConditions(22,55,253,500)
+		sleep(300)			
+	except (IOError,TypeError) as e:
+		print("Error")
+
